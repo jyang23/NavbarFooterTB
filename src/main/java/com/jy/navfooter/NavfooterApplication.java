1aa03c67231bf76789fa3f606db6822984eea503
Justin <justinyang1000@hotmail.com>
2019-07-15 09:53:45 -0400

    Initial commit

---
+++ src/main/java/com/jy/navfooter/NavfooterApplication.java
@@ -0,0 +1,13 @@
+package com.jy.navfooter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavfooterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavfooterApplication.class, args);
+    }
+
+}
